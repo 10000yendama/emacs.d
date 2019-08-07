f1457c419ea1e9d971a5a4b1f8991e9e22fd7a9f
10000yendama <10000yendama@gmail.com>
2019-08-07 23:41:23 +0900

    add init file for windows

---
+++ inits/windows-common.el
@@ -0,0 +1,13 @@
+;; windows-common.el
+
+;; font
+(when (window-system)
+  (create-fontset-from-ascii-font "Ricty:pixelsize=18:weight=regular:slant=normal" nil "ricty")
+  (add-to-list 'default-frame-alist '(font . "fontset-ricty"))
+)
+
+;; theme
+(when (window-system)
+  (use-package color-theme-sanityinc-tomorrow
+    :config
+    (load-theme 'sanityinc-tomorrow-eighties t)))
