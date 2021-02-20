b6f378197f72d2a43db2003401ab436a055616d0
10000yendama <10000yendama@gmail.com>
2021-02-20 23:59:27 +0900

    use ricty throughout all unicode codepoints

--- inits/cocoa-emacs-common.el
+++ inits/cocoa-emacs-common.el
@@ -2,8 +2,9 @@
 
 ;; font
 (when (window-system)
-  (create-fontset-from-ascii-font "Ricty:pixelsize=18:weight=regular:slant=normal" nil "ricty")
-  (set-fontset-font "fontset-ricty" 'unicode "Ricty:weight=regular:slant=normal" nil 'append)
+  (create-fontset-from-ascii-font "Ricty:pixelsize=20:weight=regular:slant=normal" nil "ricty")
+  (set-fontset-font "fontset-ricty" 'unicode
+                    "Ricty:weight=regular:slant=normal" nil 'append)
   (add-to-list 'default-frame-alist '(font . "fontset-ricty")))
 
 ;; time locate (affects Org-mode)
