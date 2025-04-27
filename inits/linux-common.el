6a23f1ac42ffbc79411c98f9c6d2ada79912fc56
10000yendama <10000yendama@gmail.com>
2025-04-27 22:54:07 +0900

    Add wl-clipboard

--- inits/linux-common.el
+++ inits/linux-common.el
@@ -26,3 +26,25 @@
         '("voicevox"
           (cmd "bash" args ("~/.local/bin/voicevox_wrapper.sh"))))
   (setq read-aloud-engine "voicevox"))
+
+(when (getenv "WSLENV")
+  (if (or (null (executable-find "wl-copy"))
+          (null (executable-find "wl-paste")))
+      (message "Installing wl-clipboard is recommended \
+to improve clipboard handling between WSL and Windows.")
+    ;; credit: yorickvP on Github
+    (defvar wl-copy-process nil)
+    (defun wl-copy (text)
+      (setq wl-copy-process (make-process :name "wl-copy"
+                                          :buffer nil
+                                          :command '("wl-copy" "-f" "-n")
+                                          :connection-type 'pipe
+                                          :noquery t))
+      (process-send-string wl-copy-process text)
+      (process-send-eof wl-copy-process))
+    (defun wl-paste ()
+      (if (and wl-copy-process (process-live-p wl-copy-process))
+          nil ; should return nil if we're the current paste owner
+        (shell-command-to-string "wl-paste -n | tr -d \r")))
+    (setq interprogram-cut-function 'wl-copy)
+    (setq interprogram-paste-function 'wl-paste)))
